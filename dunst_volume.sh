2b898ecc3c67e5f295e0554c6ad69370f77768d6
LevitatingBusinessMan <public@reinfernhout.xyz>
2021-04-05 21:38:41 +0200

    Mon Apr  5 09:38:41 PM CEST 2021

--- dunst_volume.sh
+++ dunst_volume.sh
@@ -4,4 +4,11 @@
 VOLUME=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
-dunstify "Volume: ${VOLUME}" -r 1337 -t "1000"
+ACTION=$(dunstify "Volume: ${VOLUME}" -h string:x-dunst-stack-tag:volume -t "1000" \
+	-A "open,Open Pavucontrol")
+
+if [ $ACTION == "open" ]; then
+
+	exec pavucontrol	
+
+fi
