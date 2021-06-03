97ed1281a784cdf2489ad0982679e62d97823354
LevitatingBusinessMan <public@reinfernhout.xyz>
2021-06-03 19:35:52 +0200

    Thu Jun  3 07:35:52 PM CEST 2021

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,5 +8,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar main &
+polybar code &
 
 echo "Polybar launched..."
