e511e1853325a98a4c4f78efdac50afab2ad68ee
LevitatingBusinessMan <public@reinfernhout.xyz>
2021-06-24 18:01:55 +0200

    Thu Jun 24 06:01:55 PM CEST 2021

--- dunst_volume.sh
+++ dunst_volume.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Shamelessly stolen from stackoverflow
-VOLUME=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
-    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+SINK=$( pactl list short sinks | grep RUNNING | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
+VOLUME=$( pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
 
 ACTION=$(dunstify "Volume: ${VOLUME}" -h string:x-dunst-stack-tag:volume -t "1000" \
 	-A "open,Open Pavucontrol")
