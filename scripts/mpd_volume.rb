7906156dd17cec07087efd4d87b932e17afa22df
LevitatingBusinessMan <public@reinfernhout.xyz>
2022-02-20 20:17:02 +0100

    Sun Feb 20 08:17:02 PM CET 2022

--- scripts/mpd_volume.rb
+++ scripts/mpd_volume.rb
@@ -3,6 +3,8 @@
 HOST = "192.168.1.103"
 PORT = 6600
 
+IPC = true
+
 def err str
 	STDERR.puts str
 	`notify-send mpd_volume.rb "#{str}" -u critical`
@@ -18,16 +20,7 @@ if ! first_line.start_with? "OK MPD"
 	abort err "Unexpected response: #{first_line}"
 end
 
-loop do
-	begin
-		mpd.recv_nonblock 1000, exception: false
-		mpd.puts "idle mixer"
-
-		data = mpd.recv 1000
-		if data != "changed: mixer\nOK\n"
-			err "Unexpected response: #{data}"
-		end
-
+def get_status mpd
 		mpd.puts "status"
 
 		res  = ""
@@ -39,10 +32,28 @@ loop do
 				volume = line.split(": ")[1].to_i
 				puts "New volume: #{volume}"
 				File.write "/tmp/mpd_volume", volume
+				`polybar-msg hook mpd_volume 2` if IPC
 				suc = true
 				break
 			end
 		end
+		suc
+end
+
+# Get an initial value
+get_status mpd
+
+loop do
+	begin
+		mpd.recv_nonblock 1000, exception: false
+		mpd.puts "idle mixer"
+
+		data = mpd.recv 1000
+		if data != "changed: mixer\nOK\n"
+			err "Unexpected response: #{data}"
+		end
+
+		suc = get_status mpd
 
 		err "Didn't receive volume from response: #{res}" if !suc
 
